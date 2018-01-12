d331ba5a389e312c299b236a86276ce960ddd6b1
Sundar <sundar@AMB00864.local>
2018-01-12 12:47:58 +0530

    tasks added

---
+++ ci/tasks/package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e +x
+
+pushd movie-fun-source
+  echo "Packaging WAR"
+  ./mvnw clean package -DskipTests
+popd
+
+war_count=`find movie-fun-source/target -type f -name *.war | wc -l`
+
+if [ $war_count -gt 1 ]; then
+  echo "More than one war found, don't know which one to deploy. Exiting"
+  exit 1
+fi
+
+find movie-fun-source/target -type f -name *.war -exec cp "{}" package-output/moviefun.war \;
+
+echo "Done packaging"
+exit 0
