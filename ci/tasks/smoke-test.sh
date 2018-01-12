e9c1a9bc2768e88980e38eaca3149316c72ca4e8
Sundar <sundar@AMB00864.local>
2018-01-12 17:24:58 +0530

    added smoke test util scripts

--- ci/tasks/smoke-test.sh
+++ ci/tasks/smoke-test.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-apt-get update && apt-get install -y curl
+#apt-get update && apt-get install -y curl
 
 if [ -z $MOVIE_FUN_URL ]; then
   echo "MOVIE_FUN_URL not set"
@@ -11,7 +11,8 @@ fi
 
 pushd movie-fun-source
   echo "Running smoke tests for Movie Fun deployed at $MOVIE_FUN_URL"
-  smoke-tests/bin/test $MOVIE_FUN_URL
+  #smoke-tests/bin/test $MOVIE_FUN_URL
+  ./mvnw test
 popd
 
 exit 0
