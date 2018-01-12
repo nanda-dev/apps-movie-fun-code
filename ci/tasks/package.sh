ab0b44507af5c4af00b4deddcabc666fa9ce8a48
Sundar <sundar@AMB00864.local>
2018-01-12 15:46:54 +0530

    updated name of war to reflect snapshot copy

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -14,15 +14,11 @@ if [ $war_count -gt 1 ]; then
   exit 1
 fi
 
-echo “is war file present for cp process?”
-find movie-fun-source/target -type f -name *.war > ./temp.txt
-cat ./temp.txt
+
 
 find movie-fun-source/target -type f -name *.war -exec cp "{}" package-output/moviefun.war \;
 
-echo “is war file present after cp process?”
-find movie-fun-source/target -type f -name package-output/moviefun.war > ./temp2.txt
-cat ./temp2.txt
+
 
 echo "Done packaging"
 exit 0
